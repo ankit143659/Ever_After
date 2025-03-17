825ec08f80638d5a08ba387b3c36967f4d917f8f
Ankit Singh <ankitsingh14175@gmail.com>
2025-03-17 06:22:46 +0530

    Ui

--- app/src/main/java/com/example/ever_after/login_page.kt
+++ app/src/main/java/com/example/ever_after/login_page.kt
@@ -1,5 +1,6 @@
 package com.example.ever_after
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -7,25 +8,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.airbnb.lottie.LottieAnimationView
 
-class LoginPage : AppCompatActivity() {
+class login_page : AppCompatActivity() {
     private lateinit var btnLogin : Button
-    private lateinit var btnRegister : Button
+    private lateinit var lottieAnimationView: LottieAnimationView
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_page)
-
+        setContentView(R.layout.activity_login_page2)
         btnLogin = findViewById(R.id.btnLogin)
-        btnRegister = findViewById(R.id.btnRegister)
-
+        lottieAnimationView = findViewById(R.id.lotti)
+        lottieAnimationView.playAnimation()
         btnLogin.setOnClickListener{
-            val intent = Intent(this,login_page::class.java)
-            startActivity(intent)
-        }
-
-        btnRegister.setOnClickListener{
-            val intent = Intent(this,Registration::class.java)
+            val intent = Intent(this,detailsPage::class.java)
             startActivity(intent)
         }
     }
