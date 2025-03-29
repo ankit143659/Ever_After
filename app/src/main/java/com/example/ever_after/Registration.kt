7a97e02acd06835718a225476b3c9d153c48618a
Shreya3624 <creativeshreya24@gmail.com>
2025-03-29 12:03:49 +0530

    basic change

--- app/src/main/java/com/example/ever_after/Registration.kt
+++ app/src/main/java/com/example/ever_after/Registration.kt
@@ -1,5 +1,6 @@
 package com.example.ever_after
 
+import android.annotation.SuppressLint
 import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
@@ -8,18 +9,21 @@ import android.view.View
 import android.view.Window
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.airbnb.lottie.LottieAnimationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
+import org.w3c.dom.Text
 
 class Registration : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
     private lateinit var firestore: FirebaseFirestore
     private lateinit var realtimeDb: FirebaseDatabase
+    private lateinit var Login: TextView
 
     private lateinit var name: EditText
     private lateinit var email: EditText
@@ -31,6 +35,7 @@ class Registration : AppCompatActivity() {
 
     private lateinit var loadingDialog: Dialog
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
@@ -42,6 +47,7 @@ class Registration : AppCompatActivity() {
 
         // Initialize UI Elements
         name = findViewById(R.id.name)
+        Login = findViewById(R.id.Login)
         email = findViewById(R.id.etEmail)
         phno = findViewById(R.id.etPhone)
         pass = findViewById(R.id.etPassword)
@@ -50,6 +56,10 @@ class Registration : AppCompatActivity() {
 
 
         setupLoadingDialog()
+        Login.setOnClickListener {
+            val intent = Intent(this, login_page::class.java)
+            startActivity(intent)
+        }
 
         btnRegister.setOnClickListener {
             registerUser()
